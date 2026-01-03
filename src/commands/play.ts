968c0856d95be1f25fdebf6fb14df1166aeeaff4
EPIC SID <siddhant.verma370@gmail.com>
2026-01-03 13:22:04 +0530

    Stable version playing from name, yt link, spotify link

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -1,5 +1,7 @@
 import { SlashCommandBuilder, GuildMember } from 'discord.js';
 import { distube } from '../client.js';
+// @ts-ignore
+import yts from 'yt-search';
 
 export default {
     data: new SlashCommandBuilder()
@@ -9,8 +11,9 @@ export default {
             option.setName('query')
                 .setDescription('The song you want to play (link or name)')
                 .setRequired(true)),
+
     async execute(interaction: any) {
-        const query = interaction.options.getString('query', true);
+        let query = interaction.options.getString('query', true);
         const member = interaction.member as GuildMember;
         const voiceChannel = member.voice.channel;
 
@@ -18,9 +21,24 @@ export default {
             return interaction.reply({ content: 'You need to be in a voice channel to play music!', ephemeral: true });
         }
 
-        await interaction.reply({ content: '🔍 Searching and adding to queue...', ephemeral: true });
+        await interaction.reply({ content: '🔍 Searching...', ephemeral: true });
 
         try {
+            // Check if query is a URL
+            const isUrl = /^(https?:\/\/)/.test(query);
+
+            if (!isUrl) {
+                const searchResults = await yts(query);
+                if (!searchResults || !searchResults.videos.length) {
+                    return interaction.editReply({ content: '❌ No results found on YouTube.' });
+                }
+                // Update query to the URL of the first result
+                query = searchResults.videos[0].url;
+                await interaction.editReply({ content: `🔍 Found: **${searchResults.videos[0].title}**\nAdding to queue...` });
+            } else {
+                await interaction.editReply({ content: '🔍 URL detected, adding to queue...' });
+            }
+
             await distube.play(voiceChannel, query, {
                 member: member,
                 textChannel: interaction.channel
